9e236d8c47e4ed2e21c7741e8afc82afae16d703
sappChak <konotop401@gmail.com>
2024-07-20 17:04:36 +0200

    refactor(user.module): update envFilePath and module imports

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -5,19 +5,19 @@ import { UserService } from './services/user.service';
 import { User } from './entities/user.entity';
 import { RmqModule } from '@app/shared/rmq/rmq.module';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database.module';
+import { DatabaseModule } from '@app/shared/database/database.module';
 import typeormConfig from './config/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: './apps/user/.env',
       isGlobal: true,
       load: [typeormConfig],
     }),
     DatabaseModule,
-    RmqModule,
     TypeOrmModule.forFeature([User]),
+    RmqModule,
   ],
   controllers: [UserController],
   providers: [UserService],
