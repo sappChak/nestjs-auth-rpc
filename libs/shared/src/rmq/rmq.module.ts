ce90df294e69046b05364003b46b74f4c3d372bf
sappChak <konotop401@gmail.com>
2024-07-20 17:01:40 +0200

    feat(rmq): add durable queue option and acknowledgeMessage method

--- libs/shared/src/rmq/rmq.module.ts
+++ libs/shared/src/rmq/rmq.module.ts
@@ -24,6 +24,9 @@ export class RmqModule {
               options: {
                 urls: [configService.get<string>('RABBIT_MQ_URI')],
                 queue: configService.get<string>(`RABBIT_MQ_${name}_QUEUE`),
+                queueOptions: {
+                  durable: true,
+                },
               },
             }),
             inject: [ConfigService],
--- libs/shared/src/rmq/rmq.service.ts
+++ libs/shared/src/rmq/rmq.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { RmqOptions, Transport } from '@nestjs/microservices';
+import { RmqContext, RmqOptions, Transport } from '@nestjs/microservices';
 
 @Injectable()
 export class RmqService {
   public constructor(private readonly configService: ConfigService) { }
 
   public getOptions(queueName: string, noAck: boolean = false): RmqOptions {
+    console.log(this.configService.get<string>(`RABBIT_MQ_${queueName}_QUEUE`));
     return {
       transport: Transport.RMQ,
       options: {
         urls: [this.configService.get<string>('RABBIT_MQ_URI')],
         queue: this.configService.get<string>(`RABBIT_MQ_${queueName}_QUEUE`),
+        queueOptions: {
+          durable: true,
+        },
         noAck,
         persistent: true,
       },
     };
+  }
+
+  public acknowledgeMessage(context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+    channel.ack(originalMessage);
   }
 }
