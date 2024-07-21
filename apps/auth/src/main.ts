05fc4c0fdf11ee1a47703b0f0f119fd6492c6199
sappChak <konotop401@gmail.com>
2024-07-21 09:22:43 +0200

    feat(auth-module): integrate RMQ service

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
+import { RmqService } from '@app/shared/rmq/rmq.service';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { setupMiddlewares } from './config/middlewares';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AuthModule);
-  setupMiddlewares(app);
   const configService = app.get(ConfigService);
+  setupMiddlewares(app);
+
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice<MicroserviceOptions>(rmqService.getOptions('AUTH'));
+  await app.startAllMicroservices();
+
   await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
