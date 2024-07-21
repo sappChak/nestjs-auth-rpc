2146cf43e881b4d09bfaaa0ac5b0db7d6403faa2
sappChak <konotop401@gmail.com>
2024-07-21 11:58:38 +0200

    feat(user.dto): add validation and update DTOs
    - Add IsEmail and IsStrongPassword validation to CreateUserDto
    - Update example password in CreateUserDto to meet strong password criteria
    - Create UpdateUserDto using PartialType for optional updates

--- apps/user/src/dto/create-user.dto.ts
+++ apps/user/src/dto/create-user.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsStrongPassword } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ example: 'example@example.com', description: 'User email' })
+  @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'password123', description: 'User password' })
+  @ApiProperty({ example: '!Password123$', description: 'User password' })
+  @IsStrongPassword()
   password: string;
 }
