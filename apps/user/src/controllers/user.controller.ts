3fff42727a3c6a8513fa64d0a26f8c8c5aef0f1c
sappChak <konotop401@gmail.com>
2024-07-22 23:07:31 +0200

    refactor(user.controller): reorder imports and remove logger instance

--- apps/user/src/controllers/user.controller.ts
+++ apps/user/src/controllers/user.controller.ts
@@ -1,21 +1,28 @@
-import { Controller, Logger } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { UserService } from '../services/user.service';
-import { User } from '../entities/user.entity';
-import { CreateUserDto } from '../dto/create-user.dto';
+import {
+  Controller,
+  Logger,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
 import {
   Ctx,
   MessagePattern,
   Payload,
   RmqContext,
 } from '@nestjs/microservices';
+import { ApiTags } from '@nestjs/swagger';
+import { RpcExceptionFilter } from 'apps/token/src/filters/rpc-exception.filter';
+import { LoggingInterceptor } from 'apps/token/src/interceptors/logger.interceptor';
+import { UserService } from '../services/user.service';
+import { User } from '../entities/user.entity';
+import { CreateUserDto } from '../dto/create-user.dto';
 import { RmqService } from '@app/shared/rmq/rmq.service';
 
 @ApiTags('Users')
 @Controller('users')
+@UseFilters(RpcExceptionFilter)
+@UseInterceptors(LoggingInterceptor)
 export class UserController {
-  private readonly logger = new Logger(UserController.name);
-
   public constructor(
     private readonly userService: UserService,
     private readonly rmqService: RmqService,
@@ -26,7 +33,6 @@ export class UserController {
     @Payload() email: string,
     @Ctx() context: RmqContext,
   ): Promise<User> {
-    this.logger.debug('User email: ', email);
     const user = await this.userService.getUserByEmail(email);
     this.rmqService.acknowledgeMessage(context);
     return user;
