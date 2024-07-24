bf66161bc8a56b1c015127c95c8470897ba3567a
sappChak <konotop401@gmail.com>
2024-07-24 11:33:40 +0200

    refactor(user): simplify UserController and remove unnecessary dependencies

--- apps/user/src/controllers/user.controller.ts
+++ apps/user/src/controllers/user.controller.ts
@@ -1,50 +1,22 @@
-import {
-  Controller,
-  Logger,
-  UseFilters,
-  UseInterceptors,
-} from '@nestjs/common';
-import {
-  Ctx,
-  MessagePattern,
-  Payload,
-  RmqContext,
-} from '@nestjs/microservices';
-import { ApiTags } from '@nestjs/swagger';
-import { RpcExceptionFilter } from 'apps/token/src/filters/rpc-exception.filter';
-import { LoggingInterceptor } from 'apps/token/src/interceptors/logger.interceptor';
+import { Controller, UseInterceptors } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { UserService } from '../services/user.service';
 import { User } from '../entities/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { RmqService } from '@app/shared/rmq/rmq.service';
+import { RmqInterceptor } from '@app/shared/interceptors/rmq.interceptor';
 
-@ApiTags('Users')
 @Controller('users')
-@UseFilters(RpcExceptionFilter)
-@UseInterceptors(LoggingInterceptor)
+@UseInterceptors(RmqInterceptor)
 export class UserController {
-  public constructor(
-    private readonly userService: UserService,
-    private readonly rmqService: RmqService,
-  ) { }
+  public constructor(private readonly userService: UserService) { }
 
   @MessagePattern({ cmd: 'get-user-by-email' })
-  public async getUserByEmail(
-    @Payload() email: string,
-    @Ctx() context: RmqContext,
-  ): Promise<User> {
-    const user = await this.userService.getUserByEmail(email);
-    this.rmqService.acknowledgeMessage(context);
-    return user;
+  public async getUserByEmail(@Payload() email: string): Promise<User> {
+    return await this.userService.getUserByEmail(email);
   }
 
   @MessagePattern({ cmd: 'create-user' })
-  public async createUser(
-    @Payload() user: CreateUserDto,
-    @Ctx() context: RmqContext,
-  ): Promise<User> {
-    const newUser = await this.userService.saveUser(user);
-    this.rmqService.acknowledgeMessage(context);
-    return newUser;
+  public async createUser(@Payload() user: CreateUserDto): Promise<User> {
+    return await this.userService.saveUser(user);
   }
 }
