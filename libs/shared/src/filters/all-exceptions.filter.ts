42d28646921ca67b43b82916f4f7dd3ba7243ed9
sappChak <konotop401@gmail.com>
2024-07-24 11:15:07 +0200

    feat(shared): add global exception filter

---
+++ libs/shared/src/filters/all-exceptions.filter.ts
@@ -0,0 +1,10 @@
+import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    response.status(exception.status).json(exception.response);
+  }
+}
