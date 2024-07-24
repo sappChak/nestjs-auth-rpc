24ac93e342ae6d7d87ca76f9d4479351374ecb8e
sappChak <konotop401@gmail.com>
2024-07-24 11:32:58 +0200

    style(user): reoder imports and remove comments

--- apps/user/src/services/user.service.ts
+++ apps/user/src/services/user.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Nullable } from '@app/shared/types/types';
 import { User } from '../entities/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { Nullable } from '@app/shared/types/types';
 
 @Injectable()
 export class UserService {
@@ -19,12 +19,9 @@ export class UserService {
     let existingUser = await this.userRepository.findOne({
       where: { email: user.email },
     });
-
     if (existingUser) {
-      // If user exists, update the existing user with new data
       existingUser = this.userRepository.merge(existingUser, user);
     } else {
-      // If user doesn't exist, create a new user
       existingUser = this.userRepository.create(user);
     }
 
