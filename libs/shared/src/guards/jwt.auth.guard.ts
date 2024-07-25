f72019c3fd50a7c9fedb8d5f9163fc4e4367f2ab
sappChak <konotop401@gmail.com>
2024-07-25 15:43:33 +0200

    refactor(jwt-auth-guard): improve token validation and error messages

--- libs/shared/src/guards/jwt.auth.guard.ts
+++ libs/shared/src/guards/jwt.auth.guard.ts
@@ -7,11 +7,13 @@ import {
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
-import { AUTH_SERVICE } from '../constants/constants';
+import { TOKEN_SERVICE } from '../constants/constants';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-        public constructor(@Inject(AUTH_SERVICE)private readonly tokenClient: ClientProxy) { }
+        public constructor(
+                @Inject(TOKEN_SERVICE) private readonly tokenClient: ClientProxy,
+        ) { }
 
         public canActivate(
                 context: ExecutionContext,
@@ -19,14 +21,15 @@ export class JwtAuthGuard implements CanActivate {
                 const request = context.switchToHttp().getRequest();
                 const authorization = request.headers['authorization'];
 
-                if (!authorization) {
-                        throw new UnauthorizedException('Token not provided');
-                }
+                if (!authorization)
+                        throw new UnauthorizedException('Access token is not provided');
+
                 const [bearer, token] = authorization.split(' ');
-                if (bearer !== 'Bearer' || !token) {
-                        throw new UnauthorizedException('Invalid token');
-                }
+                if (bearer !== 'Bearer' || !token)
+                        throw new UnauthorizedException('Invalid access token');
+
                 request.user = this.tokenClient.send('verify-access-token', token);
+
                 return true;
         }
 }
