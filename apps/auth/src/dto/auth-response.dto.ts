adbedb5f3430f8a6c3671566d9b466544e64bcb6
sappChak <konotop401@gmail.com>
2024-07-26 13:52:58 +0200

    refactor: modify serialization strategy

--- apps/auth/src/dto/auth-response.dto.ts
+++ apps/auth/src/dto/auth-response.dto.ts
@@ -1,16 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Exclude, Expose } from 'class-transformer';
+import { Exclude } from 'class-transformer';
 
 @Exclude()
 export class AuthResponseDto {
   @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' })
-  @Expose()
   accessToken: string;
 
+  @Exclude()
   refreshToken: string;
 
   @ApiProperty({ example: { id: 1, email: 'example@example.com' } })
-  @Expose()
   user: {
     id: number;
     name: string;
--- apps/user/src/entities/user.entity.ts
+++ apps/user/src/entities/user.entity.ts
@@ -1,29 +1,24 @@
-import { Expose } from 'class-transformer';
+import { Exclude } from 'class-transformer';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  @Expose()
   id: number;
 
   @Column()
-  @Expose()
   name: string;
 
   @Column()
-  @Expose()
   surname: string;
 
   @Column()
-  @Expose()
   email: string;
 
   @Column({ nullable: true })
-  @Expose()
   profile_picture: string;
 
   @Column()
-  @Expose()
+  @Exclude()
   password: string;
 }
