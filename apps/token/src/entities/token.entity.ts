76b17467982c01d18b98bd1dea94afea513d84ba
sappChak <konotop401@gmail.com>
2024-07-26 14:18:09 +0200

    refactor(token-entity): remove unnecessary Expose decorators

--- apps/token/src/entities/token.entity.ts
+++ apps/token/src/entities/token.entity.ts
@@ -1,16 +1,13 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-import { Expose } from 'class-transformer';
 
 @Entity()
 export class RefreshToken {
   @PrimaryGeneratedColumn()
-  @Expose()
   id: number;
 
   @Column({ type: 'text' })
   refresh_token: string;
 
   @Column({ type: 'int' })
-  @Expose()
   user_id: number;
 }
