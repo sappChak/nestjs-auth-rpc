9094551fe1fbc424b1cf46f87432589aef10594f
sappChak <konotop401@gmail.com>
2024-07-27 20:51:37 +0200

    style(validate-refresh): remove unnecessary braces in if statements

--- apps/gateway/src/pipes/validate-refresh.pipe.ts
+++ apps/gateway/src/pipes/validate-refresh.pipe.ts
@@ -3,14 +3,12 @@ import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 @Injectable()
 export class ValidateRefreshTokenPipe implements PipeTransform {
   transform(refreshToken: any) {
-    if (!refreshToken) {
+    if (!refreshToken)
       throw new BadRequestException('Refresh token is missing');
-    }
 
     const tokenRegex = /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/;
-    if (!tokenRegex.test(refreshToken)) {
+    if (!tokenRegex.test(refreshToken))
       throw new BadRequestException('Invalid refresh token format');
-    }
 
     return refreshToken;
   }
