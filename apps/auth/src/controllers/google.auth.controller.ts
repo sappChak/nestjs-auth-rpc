6980bf742abc975f9497be2bf3b9fb0b41bcd0d6
Andrii Konotop <konotop401@gmail.com>
2024-08-02 13:14:26 +0200

    feat(auth): refactor Google authentication flow
    - Simplified the Google authentication flow in `GoogleAuthController` by delegating the logic to `GoogleAuthService`.
    - Removed redundant methods (`getOAuthTokens`, `getGoogleUser`, `createUserDto`, `loginGoogleUser`) from the controller.
    - Updated `handleGoogleAuthLogin` method to `handleLoginWithGoogle` and refactored it to use `authenticateWithGoogle` method from `GoogleAuthService`.

--- apps/auth/src/controllers/google.auth.controller.ts
+++ apps/auth/src/controllers/google.auth.controller.ts
@@ -1,9 +1,4 @@
-import {
-  BadRequestException,
-  Controller,
-  UnauthorizedException,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { RmqInterceptor } from '@app/shared/interceptors/rmq.interceptor';
 import { GoogleAuthService } from '../services/google.auth.service';
@@ -15,43 +10,9 @@ export class GoogleAuthController {
   public constructor(private readonly googleAuthService: GoogleAuthService) { }
 
   @MessagePattern({ cmd: 'login-with-google' })
-  public async handleGoogleAuthLogin(
+  public async handleLoginWithGoogle(
     @Payload() data: { code: string; state: string },
-  ) {
-    const oAuthTokens = await this.getOAuthTokens(data.code);
-    const googleUser = await this.getGoogleUser(oAuthTokens);
-    const userDto = this.createUserDto(googleUser);
-    return await this.loginGoogleUser(userDto);
-  }
-
-  private async getOAuthTokens(code: string) {
-    const oAuthTokens = await this.googleAuthService.getGoogleOAuthTokens(code);
-    if (!oAuthTokens) throw new BadRequestException('Invalid OAuth tokens');
-    return oAuthTokens;
-  }
-
-  private async getGoogleUser(oAuthTokens: any) {
-    const googleUser = await this.googleAuthService.getGoogleUser(
-      oAuthTokens.id_token,
-      oAuthTokens.access_token,
-    );
-    if (!googleUser) throw new UnauthorizedException('Google user not found');
-    return googleUser;
-  }
-
-  private createUserDto(googleUser: any) {
-    return {
-      name: googleUser.given_name,
-      surname: googleUser.family_name,
-      email: googleUser.email,
-      picture: googleUser.picture,
-      password: process.env.GOOGLE_PASSWORD!,
-    };
-  }
-
-  private async loginGoogleUser(userDto: any): Promise<AuthResponseDto> {
-    const result = await this.googleAuthService.loginGoogleUser(userDto);
-    if (!result) throw new UnauthorizedException('Invalid credentials');
-    return result;
+  ): Promise<AuthResponseDto> {
+    return this.googleAuthService.authenticateWithGoogle(data.code, data.state);
   }
 }
