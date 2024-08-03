310ad96fba9a380ecdfd8f0bb4d297baf1832e70
Andrii Konotop <konotop401@gmail.com>
2024-08-03 20:28:30 +0200

    refactor(gateway): optimize main.ts for performance and readability

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 import { GatewayModule } from './gateway.module';
 import { setupMiddlewares } from './config/middlewares';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(GatewayModule);
+  const port = app.get(ConfigService).get('PORT');
   setupMiddlewares(app);
-  await app.listen(8080);
+  await app.listen(port);
 }
 bootstrap();
