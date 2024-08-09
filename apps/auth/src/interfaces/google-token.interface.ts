74ee782aad2ab60d7875884191afe2df94b2b5e5
Andrii Konotop <konotop401@gmail.com>
2024-08-09 19:00:46 +0200

    refactor: remove auth microservice

--- apps/auth/src/interfaces/google-token.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface GoogleToken {
-  access_token: string;
-  id_token: string;
-  expires_in: number;
-  refresh_token: string;
-  scope: string;
-  token_type: string;
-}
--- libs/shared/src/constants/constants.ts
+++ libs/shared/src/constants/constants.ts
@@ -1,3 +1,2 @@
 export const USER_SERVICE = 'USER';
 export const TOKEN_SERVICE = 'TOKEN';
-export const AUTH_SERVICE = 'AUTH';
